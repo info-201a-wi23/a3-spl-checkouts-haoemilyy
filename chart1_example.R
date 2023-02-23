d5019f57e18f94f46000da9aa89e9c9c21271499
haoemilyy <haoemily@uw.edu>
2023-02-23 11:59:13 -0800

    Fix varying title names in chart1

--- chart1_example.R
+++ chart1_example.R
@@ -13,15 +13,21 @@ spl_df$date <- as.Date(spl_df$date, format = "%Y-%m-%d")
 Dickens_df <- spl_df %>% 
   filter(str_detect(Creator, "Dickens"))
 
+Dickens_df$Title[str_detect(Dickens_df$Title, "Oliver Twist")] <- "Oliver Twist"
+Dickens_df$Title[str_detect(Dickens_df$Title, "Great Expectations")] <- "Great Expectations"
+Dickens_df$Title[str_detect(Dickens_df$Title, "A Tale of Two Cities")] <- "A Tale of Two Cities"
+Dickens_df$Title[str_detect(Dickens_df$Title, "A Christmas Carol")] <- "A Christmas Carol"
+Dickens_df$Title[str_detect(Dickens_df$Title, "David Copperfield")] <- "David Copperfield"
+
 Dickens_titles <- Dickens_df %>% 
   filter(Title %in% 
-           c("Oliver Twist", "Great Expectations", "A Tale of Two Cities"))
+           c("Oliver Twist", "Great Expectations", "A Tale of Two Cities", "A Christmas Carol", "David Copperfield"))
 
 Dickens_checkouts_df <- Dickens_titles %>% 
   group_by(date, Title) %>% 
   summarize(Dickens_total_checkouts = sum(Checkouts))
 
-Dickens_plot <- ggplot(data = Dickens_checkouts_df) +
+ggplot(data = Dickens_checkouts_df) +
   geom_line(aes(x = date,
                 y = Dickens_total_checkouts,
                 color = Title)) +
