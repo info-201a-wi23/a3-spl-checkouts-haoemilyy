4e885a821d19a05393f41e20b28a2dcc218af91c
haoemilyy <haoemily@uw.edu>
2023-02-22 23:55:08 -0800

    Complete summary.R

---
+++ summary.R
@@ -0,0 +1,120 @@
+# Summary
+library("dplyr")
+library("stringr")
+library("ggplot2")
+library("plotly")
+
+spl_df <- read.csv("~/2022-2023-All-Checkouts-SPL-Data.csv/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+spl_df <- spl_df %>% 
+  mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+spl_df$date <- as.Date(spl_df$date, format = "%Y-%m-%d")
+
+Dickens_df <- spl_df %>% 
+  filter(str_detect(Creator, "Dickens"))
+
+# Which digital book had the most checkouts and in which year and month?
+digital_most_title_monthly <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  filter(Checkouts == max(Checkouts)) %>% 
+  pull(Title)
+
+digital_most_month_checkout <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  filter(Checkouts == max(Checkouts)) %>% 
+  pull(date)
+
+digital_most_monthly_checkout <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  filter(Checkouts == max(Checkouts)) %>% 
+  pull(Checkouts)
+
+# which physical book had the most checkouts and when?
+physical_most_title_checkout <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  filter(Checkouts == max(Checkouts)) %>% 
+  pull(Title)
+
+physical_most_month_checkout <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  filter(Checkouts == max(Checkouts)) %>% 
+  pull(date)
+
+physical_most_monthly_checkout <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  filter(Checkouts == max(Checkouts)) %>% 
+  pull(Checkouts)
+
+# Which digital book has the least amount of checkouts, and which digital book has the most amount of checkouts?
+# most
+digital_most_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == max(total_checkouts)) %>% 
+  pull(total_checkouts)
+
+digital_title_most_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == max(total_checkouts)) %>% 
+  pull(Title)
+
+# least
+digital_least_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == min(total_checkouts)) %>% 
+  pull(total_checkouts)
+
+digital_title_least_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == min(total_checkouts)) %>% 
+  pull(Title)
+
+# Which physical book has the most amount of checkouts, and which physical book has the least amount of checkouts?
+# most
+physical_most_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == max(total_checkouts)) %>% 
+  pull(total_checkouts)
+
+physical_title_most_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == max(total_checkouts)) %>% 
+  pull(Title)
+
+# least
+physical_least_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == min(total_checkouts)) %>% 
+  pull(total_checkouts)
+
+physical_title_least_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  group_by(Title) %>% 
+  summarize(total_checkouts = sum(Checkouts)) %>% 
+  filter(total_checkouts == min(total_checkouts)) %>% 
+  pull(Title)
+
+# How many total digital Charles Dickens books were checked out from 2022-23?
+digital_book_total_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Digital", na.rm = TRUE) %>% 
+  summarize(total_digital_checkouts = sum(Checkouts, na.rm = TRUE)) %>% 
+  pull(total_digital_checkouts)
+
+# How many total physical Charles Dickens books were checked out from 2022-23?
+print_book_total_checkouts <- Dickens_df %>% 
+  filter(UsageClass == "Physical", na.rm = TRUE) %>% 
+  summarize(total_physical_checkouts = sum(Checkouts, na.rm = TRUE)) %>% 
+  pull(total_physical_checkouts)
