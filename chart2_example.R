332aa2f0f51426128d33d2a86424132fb2bd14fa
haoemilyy <haoemily@uw.edu>
2023-02-22 17:50:50 -0800

    Complete and embed chart2

--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,28 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+# Data Summary
+library("dplyr")
+library("stringr")
+library("ggplot2")
+library("plotly")
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+spl_df <- read.csv("~/2022-2023-All-Checkouts-SPL-Data.csv/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+spl_df <- spl_df %>% 
+  mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+spl_df$date <- as.Date(spl_df$date, format = "%Y-%m-%d")
+
+Dickens_df <- spl_df %>% 
+  filter(str_detect(Creator, "Dickens"))
+
+Dickens_type_checkouts_df <- Dickens_df %>% 
+  group_by(date, MaterialType) %>% 
+  summarize(Dickens_total_checkouts = sum(Checkouts))
+
+material_plot <- ggplot(data = Dickens_type_checkouts_df) +
+  geom_line(aes(x = date,
+                y = Dickens_total_checkouts,
+                color = MaterialType)) +
+  scale_color_brewer(palette = "Set2") +
+    labs(title = "Comparing the Material Type of Checkout for Charles Dicken's Books",
+       x = "Date",
+       y = "Total Checkouts") +
+  guides(color = guide_legend(title = "Material Type"))
