3ce4e64927e283f3823dd130354c53e11c8803c7
haoemilyy <haoemily@uw.edu>
2023-02-22 17:34:51 -0800

    Create chart1

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,31 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+# Data Summary
+library("dplyr")
+library("stringr")
+library("ggplot2")
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+spl_df <- read.csv("~/2022-2023-All-Checkouts-SPL-Data.csv/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+spl_df <- spl_df %>% 
+  mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+spl_df$date <- as.Date(spl_df$date, format = "%Y-%m-%d")
+
+Dickens_df <- spl_df %>% 
+  filter(str_detect(Creator, "Dickens"))
+
+Dickens_titles <- Dickens_df %>% 
+  filter(Title %in% 
+           c("Oliver Twist", "Great Expectations", "A Tale of Two Cities"))
+
+Dickens_checkouts_df <- Dickens_titles %>% 
+  group_by(date, Title) %>% 
+  summarize(Dickens_total_checkouts = sum(Checkouts))
+
+ggplot(data = Dickens_checkouts_df) +
+  geom_line(aes(x = date,
+                y = Dickens_total_checkouts,
+                color = Title)) +
+  scale_color_brewer(palette = "Set2") +
+  labs(title = "Comparing Charles Dickens' Book Title Digital Checkouts",
+       x = "Date",
+       y = "Total Checkouts") +
+  guides(color = guide_legend(title = "Book Title"))  
